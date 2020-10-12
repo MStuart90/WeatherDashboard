d08b22330990ed65ddfde03cecc2296cc719a305
Melanie Stuart <mmooradian90@yahoo.com>
2020-10-11 20:47:12 -0400

    GETing response from API for weather conditions via ajax.

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 $(document).ready(function () {
     var searches = JSON.parse(localStorage.getItem("searches")) || [];
  
+    // $( "#currentDay" ).text(currentDay);
+    var cityName = $("#city");
+    var temp = $("#temp");
+    var feelsLike = $("#feelsLike");
+    var humidity = $("#humidity");
+    var wind = $("#wind");
+    var uv = $("#uv");
 
 
     $("form").on("submit", function (event) {
         event.preventDefault();
  
         // Retrieve city value from input
-        var city = $("#search").val().trim();
+        var citySearch = $("#search").val().trim();
 
         $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=7e5813874d0192f2d42018c690376afc",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&units=imperial&appid=7e5813874d0192f2d42018c690376afc",
             method: "GET"
         }).then(function (response) {
+            console.log(response); 
+            cityName.text(response.name)
+            temp.text(response.main.temp)
+            feelsLike.text(response.main.feelsLike)
+            humidity.text(response.main.humidity)
+            wind.text(response.main.wind)
+            uv.text(response.main.uv) 
+
+
             // Add city to search history
             if (!searches.includes(response.name)) {
                 searches.push(response.name);
             }
             localStorage.setItem("searches", JSON.stringify(searches));
             $("#search").val("");
-            renderHistory();
+            // renderHistory();
+        })
+    })
+})
+
+
